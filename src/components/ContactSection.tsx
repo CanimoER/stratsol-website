183fc91ac6048b0d99dd35eca68aaeaca49bc8d5
CanimoER <flubums@gmail.com>
2025-03-27 15:35:05 -0400

    Fix form submission with static success page and Netlify configuration

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -53,8 +53,8 @@ const ContactSection = () => {
             <form 
               name="stratsol-contact"
               method="POST"
-              action="/thank-you"
-              data-netlify="true"
+              action="/success"
+              data-netlify="true" 
               netlify-honeypot="bot-field"
               className="space-y-6"
             >
